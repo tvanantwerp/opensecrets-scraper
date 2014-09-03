d411cf3c69e29a9a2d219bd04a94f299e50e7a42
Tom VanAntwerp <vanantwerp@gmail.com>
2014-09-03 15:26:37 -0400

    Writes first page of results to csv file

--- opensecrets-scrape.py
+++ opensecrets-scrape.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+import csv
 
 url = 'https://www.opensecrets.org/indivs/search.php?&name=&employ=walmart&cand=&state=&cycle=All&soft=&zip=&sort=R&page=1'
 
@@ -8,9 +9,12 @@ response = requests.get(url)
 soup = bs4.BeautifulSoup(response.text)
 rows = soup.find('div', id='tabwrap').tbody.find_all('tr')
 
-for row in rows:
-	tds = row('td')
-	cells = []
-	for td in tds:
-		cells += td.contents
-	print cells
+with open('secrets.csv', 'wb') as csvfile:
+	writer = csv.writer(csvfile)
+	output = []
+	for row in rows:
+		tds = row('td')
+		cells = []
+		for td in tds:
+			cells += td.contents
+		writer.writerow(cells)
