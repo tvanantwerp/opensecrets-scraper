e6a3b1e8f5d893c39e435804428ca6a0cf94e313
Tom VanAntwerp <vanantwerp@gmail.com>
2014-09-03 15:34:49 -0400

    Refactor to separate parsing and writing

--- opensecrets-scrape.py
+++ opensecrets-scrape.py
@@ -9,12 +9,16 @@ response = requests.get(url)
 soup = bs4.BeautifulSoup(response.text)
 rows = soup.find('div', id='tabwrap').tbody.find_all('tr')
 
+output = []
+
+for row in rows:
+	tds = row('td')
+	cells = []
+	for td in tds:
+		cells += td.contents
+	output.append(cells)
+
 with open('secrets.csv', 'wb') as csvfile:
 	writer = csv.writer(csvfile)
-	output = []
-	for row in rows:
-		tds = row('td')
-		cells = []
-		for td in tds:
-			cells += td.contents
-		writer.writerow(cells)
+	for line in output:
+		writer.writerow(line)
