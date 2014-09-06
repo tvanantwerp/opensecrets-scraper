79e5337724d3c9660a6bfaac2c78c33fe9ed0f04
Tom VanAntwerp <vanantwerp@gmail.com>
2014-09-06 09:55:30 -0400

    Retrieve all pages and write results to CSV

--- opensecrets-scrape.py
+++ opensecrets-scrape.py
@@ -5,29 +5,35 @@ import urllib
 
 initialParams = {
 	'employ': 'walmart',
-	'page': 1
+	'page': 1,
+	'totalPages': 9999
 }
-
-totalPages = 1
 
 output = []
 
 baseurl = 'https://www.opensecrets.org/indivs/search.php?&name=&cand=&state=&cycle=All&soft=&zip=&sort=R&'
-url = baseurl + urllib.urlencode(params)
+url = baseurl + urllib.urlencode(initialParams)
 
 response = requests.get(url)
 
 soup = bs4.BeautifulSoup(response.text)
-rows = soup.find('div', id='tabwrap').tbody.find_all('tr')
 pages = soup.find('div', 'pageCtrl').find_all('a')
-totalPages = int(pages[len(pages)-2].contents[0])
+initialParams['totalPages'] = int(pages[len(pages)-2].contents[0])
 
-for row in rows:
-	tds = row('td')
-	cells = []
-	for td in tds:
-		cells += td.contents
-	output.append(cells)
+while (initialParams['page'] <= initialParams['totalPages']):
+	url = baseurl + urllib.urlencode(initialParams)
+	response = requests.get(url)
+	soup = bs4.BeautifulSoup(response.text)
+	rows = soup.find('div', id='tabwrap').tbody.find_all('tr')
+	
+	for row in rows:
+		tds = row('td')
+		cells = []
+		for td in tds:
+			cells += td.contents
+		output.append(cells)
+
+	initialParams['page'] += 1
 
 with open('secrets.csv', 'wb') as csvfile:
 	writer = csv.writer(csvfile)
